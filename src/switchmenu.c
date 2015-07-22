bddbf33e19ce84f86bdf0592d5d2a9214e48c923
Rodolfo García Peñas (kix) <kix@kix.es>
2015-07-22 21:51:29 +0200

    SM: New function switchmenu_additem
    The new function switchmenu_additem do the code for:
    
    UpdateSwitchMenu(virtual_screen *vscr, WWindow *wwin, ACTION_ADD)

--- src/switchmenu.c
+++ src/switchmenu.c
@@ -171,15 +171,73 @@ static int menuIndexForWindow(WMenu * menu, WWindow * wwin, int old_pos)
 	return idx;
 }
 
+void switchmenu_additem(virtual_screen *vscr, WWindow *wwin)
+{
+	WMenu *switchmenu = vscr->menu.switch_menu;
+	WMenuEntry *entry;
+	char *t, title[MAX_MENU_TEXT_LENGTH + 6];
+	int idx = -1, tmp, len = sizeof(title);
+
+	if (!vscr->menu.switch_menu)
+		return;
+
+	if (wwin->flags.internal_window ||
+	    WFLAGP(wwin, skip_window_list) ||
+	    IS_GNUSTEP_MENU(wwin))
+		return;
+
+	if (wwin->frame->title)
+		snprintf(title, len, "%s", wwin->frame->title);
+	else
+		snprintf(title, len, "%s", DEF_WINDOW_TITLE);
+
+	if (!IS_OMNIPRESENT(wwin))
+		idx = menuIndexForWindow(switchmenu, wwin, -1);
+
+	t = ShrinkString(vscr->screen_ptr->menu_entry_font, title, MAX_WINDOWLIST_WIDTH);
+	entry = wMenuInsertCallback(switchmenu, idx, t, focusWindow, wwin);
+	wfree(t);
+
+	entry->flags.indicator = 1;
+	entry->rtext = wmalloc(MAX_WORKSPACENAME_WIDTH + 8);
+	if (IS_OMNIPRESENT(wwin))
+		snprintf(entry->rtext, MAX_WORKSPACENAME_WIDTH, "[*]");
+	else
+		snprintf(entry->rtext, MAX_WORKSPACENAME_WIDTH, "[%s]",
+			 vscr->workspace.array[wwin->frame->workspace]->name);
+
+	if (wwin->flags.hidden) {
+		entry->flags.indicator_type = MI_HIDDEN;
+		entry->flags.indicator_on = 1;
+	} else if (wwin->flags.miniaturized) {
+		entry->flags.indicator_type = MI_MINIWINDOW;
+		entry->flags.indicator_on = 1;
+	} else if (wwin->flags.focused) {
+		entry->flags.indicator_type = MI_DIAMOND;
+		entry->flags.indicator_on = 1;
+	} else if (wwin->flags.shaded) {
+		entry->flags.indicator_type = MI_SHADED;
+		entry->flags.indicator_on = 1;
+	}
+
+	wMenuRealize(switchmenu);
+
+	tmp = switchmenu->frame->top_width + 5;
+	/* if menu got unreachable, bring it to a visible place */
+	if (switchmenu->frame_x < tmp - (int) switchmenu->frame->core->width)
+		wMenuMove(switchmenu, tmp - (int) switchmenu->frame->core->width,
+			  switchmenu->frame_y, False);
+
+	wMenuPaint(switchmenu);
+}
+
 /* Update switch menu */
 void UpdateSwitchMenu(virtual_screen *vscr, WWindow *wwin, int action)
 {
 	WMenu *switchmenu = vscr->menu.switch_menu;
 	WMenuEntry *entry;
 	char title[MAX_MENU_TEXT_LENGTH + 6];
-	int len = sizeof(title);
-	int i;
-	int checkVisibility = 0;
+	int i, checkVisibility = 0;
 
 	if (!vscr->menu.switch_menu)
 		return;
@@ -193,52 +251,8 @@ void UpdateSwitchMenu(virtual_screen *vscr, WWindow *wwin, int action)
 	 *    4.  When a window changes its workspace.
 	 */
 	if (action == ACTION_ADD) {
-		char *t;
-		int idx;
-
-		if (wwin->flags.internal_window || WFLAGP(wwin, skip_window_list) || IS_GNUSTEP_MENU(wwin))
-			return;
-
-		if (wwin->frame->title)
-			snprintf(title, len, "%s", wwin->frame->title);
-		else
-			snprintf(title, len, "%s", DEF_WINDOW_TITLE);
-
-		t = ShrinkString(vscr->screen_ptr->menu_entry_font, title, MAX_WINDOWLIST_WIDTH);
-
-		if (IS_OMNIPRESENT(wwin))
-			idx = -1;
-		else {
-			idx = menuIndexForWindow(switchmenu, wwin, -1);
-		}
-
-		entry = wMenuInsertCallback(switchmenu, idx, t, focusWindow, wwin);
-		wfree(t);
-
-		entry->flags.indicator = 1;
-		entry->rtext = wmalloc(MAX_WORKSPACENAME_WIDTH + 8);
-		if (IS_OMNIPRESENT(wwin))
-			snprintf(entry->rtext, MAX_WORKSPACENAME_WIDTH, "[*]");
-		else
-			snprintf(entry->rtext, MAX_WORKSPACENAME_WIDTH, "[%s]",
-				 vscr->workspace.array[wwin->frame->workspace]->name);
-
-		if (wwin->flags.hidden) {
-			entry->flags.indicator_type = MI_HIDDEN;
-			entry->flags.indicator_on = 1;
-		} else if (wwin->flags.miniaturized) {
-			entry->flags.indicator_type = MI_MINIWINDOW;
-			entry->flags.indicator_on = 1;
-		} else if (wwin->flags.focused) {
-			entry->flags.indicator_type = MI_DIAMOND;
-			entry->flags.indicator_on = 1;
-		} else if (wwin->flags.shaded) {
-			entry->flags.indicator_type = MI_SHADED;
-			entry->flags.indicator_on = 1;
-		}
-
-		wMenuRealize(switchmenu);
-		checkVisibility = 1;
+		switchmenu_additem(vscr, wwin);
+		return;
 	} else {
 		char *t;
 		for (i = 0; i < switchmenu->entry_no; i++) {
