acda43ff6655eb738f98e07f5a63a0643f269799
Rodolfo García Peñas (kix) <kix@kix.es>
2018-01-01 23:05:32 +0100

    cm: check that menu exists in clip_icon_mouse_down
    This patch checks that the menu exists in the function clip_icon_mouse_down before
    unmap it.

--- src/dock.c
+++ src/dock.c
@@ -5685,7 +5685,7 @@ static void clip_icon_mouse_down(WObjDescriptor *desc, XEvent *event)
 
 	vscr->last_dock = dock;
 
-	if (dock->menu->flags.mapped)
+	if (dock->menu && dock->menu->flags.mapped)
 		wMenuUnmap(dock->menu);
 
 	if (IsDoubleClick(vscr, event)) {
