8c11be5f3388f0d57c608feb4a75f75643a46bbc
Rodolfo García Peñas (kix) <kix@kix.es>
2018-08-21 17:15:10 +0200

    frm: Remove boolean argument in reconfigure
    The boolean argument is removed in reconfigure function.

--- src/framewin.c
+++ src/framewin.c
@@ -100,7 +100,7 @@ static void remakeTexture_resizebar(WFrameWindow *fwin, int state);
 static char *get_title(WFrameWindow *fwin);
 static void paint_title(WFrameWindow *fwin, int lofs, int rofs, int state);
 static void reconfigure(WFrameWindow *fwin, int x, int y,
-			int width, int height, Bool dontMove);
+			int width, int height);
 static void handleExpose(WObjDescriptor *desc, XEvent *event);
 static void checkTitleSize(WFrameWindow *fwin);
 static void paint_button_pushed(WFrameWindow *fwin, WCoreWindow *button,
@@ -1364,7 +1364,8 @@ void wFrameWindowPaint(WFrameWindow *fwin)
 
 void wFrameWindowConfigure(WFrameWindow *fwin, int x, int y, int width, int height)
 {
-	reconfigure(fwin, x, y, width, height, False);
+	XMoveResizeWindow(dpy, fwin->core->window, x, y, width, height);
+	reconfigure(fwin, x, y, width, height);
 }
 
 static void reconfigure_titlebar(WFrameWindow *fwin, int width)
@@ -1457,14 +1458,9 @@ static void reconfigure_resizebar(WFrameWindow *fwin)
 }
 
 static void reconfigure(WFrameWindow *fwin, int x, int y,
-			int width, int height, Bool dontMove)
+			int width, int height)
 {
 	int resizedHorizontally = 0;
-
-	if (dontMove)
-		XResizeWindow(dpy, fwin->core->window, width, height);
-	else
-		XMoveResizeWindow(dpy, fwin->core->window, x, y, width, height);
 
 	if (fwin->width != width) {
 		fwin->flags.need_texture_remake = 1;
@@ -1483,7 +1479,8 @@ static void reconfigure(WFrameWindow *fwin, int x, int y,
 
 void wFrameWindowResize(WFrameWindow *fwin, int width, int height)
 {
-	reconfigure(fwin, 0, 0, width, height, True);
+	XResizeWindow(dpy, fwin->core->window, width, height);
+	reconfigure(fwin, 0, 0, width, height);
 }
 
 int wFrameWindowChangeTitle(WFrameWindow *fwin, const char *new_title)
