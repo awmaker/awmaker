a3199d9b413e4bcf045db4a8ff3115625d81a251
Rodolfo García Peñas (kix) <kix@kix.es>
2019-06-20 12:07:15 +0200

    Sync wrlib with upstream
    This patch syncs the wrlib with the upstream.
    
    Only some empty lines added.

--- wrlib/gradient.c
+++ wrlib/gradient.c
@@ -142,12 +142,15 @@ static inline unsigned char *renderGradientWidth(unsigned char *ptr, unsigned wi
 		*ptr++ = r;
 		*ptr++ = g;
 		*ptr++ = b;
+
 		*ptr++ = r;
 		*ptr++ = g;
 		*ptr++ = b;
+
 		*ptr++ = r;
 		*ptr++ = g;
 		*ptr++ = b;
+
 		*ptr++ = r;
 		*ptr++ = g;
 		*ptr++ = b;
